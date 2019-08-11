7ec2cd2869335fee815ec305a73b8390b77551d8
Халилов Рустам <konversys@gmail.com>
2019-08-12 00:33:23 +0700

    Initial commit

---
+++ app/src/main/java/ru/konverdev/pdf417reader/MainActivity.java
@@ -0,0 +1,139 @@
+package ru.konverdev.pdf417reader;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.frame.Frame;
+import com.otaliastudios.cameraview.frame.FrameProcessor;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    CameraView cameraView;
+    boolean isDetected;
+    Button btn_start;
+
+    FirebaseVisionBarcodeDetectorOptions options;
+    FirebaseVisionBarcodeDetector detector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
+                .withListener(new MultiplePermissionsListener(){
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        setupCamera();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        Toast.makeText(MainActivity.this, "Permisions denied", Toast.LENGTH_SHORT).show();
+                    }
+                }).check();
+
+    }
+
+    private void setupCamera() {
+        btn_start = findViewById(R.id.btn_again);
+        btn_start.setEnabled(isDetected);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isDetected = !isDetected;
+            }
+        });
+        cameraView = findViewById(R.id.cameraView);
+        cameraView.setLifecycleOwner(this);
+        cameraView.addFrameProcessor(new FrameProcessor() {
+            @Override
+            public void process(@NonNull Frame frame) {
+                processImage(getVisionImageFromFrame(frame));
+            }
+        });
+        options = new FirebaseVisionBarcodeDetectorOptions.Builder()
+                .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_ALL_FORMATS).build();
+        detector = FirebaseVision.getInstance().getVisionBarcodeDetector(options);
+    }
+
+    private void processImage(FirebaseVisionImage image) {
+        if (!isDetected) {
+            detector.detectInImage(image)
+                    .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                        @Override
+                        public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+                            processResult(firebaseVisionBarcodes);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    private void processResult(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+        if (!firebaseVisionBarcodes.isEmpty()) {
+            isDetected = true;
+            btn_start.setEnabled(isDetected);
+            for (FirebaseVisionBarcode barcode : firebaseVisionBarcodes) {
+                createDialog(barcode.getRawValue());
+            }
+        }
+    }
+
+    private void createDialog(String raw) {
+        Context context;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(raw)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private FirebaseVisionImage getVisionImageFromFrame(Frame frame) {
+        byte[] data = frame.getData();
+        FirebaseVisionImageMetadata metadata = new FirebaseVisionImageMetadata.Builder()
+                .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+                .setHeight(frame.getSize().getHeight())
+                .setWidth(frame.getSize().getWidth())
+                .build();
+        return FirebaseVisionImage.fromByteArray(data, metadata);
+    }
+}
